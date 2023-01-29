11806f3384429b995522b977325e8f0e5358e6a4
aaa <diego.oniarti@studenti.unitn.it>
2023-01-29 17:46:14 +0100

    cambiato play ora i singoli processi mettono in coda le canzoni ora il bot inizia a suonare appena trova la prima canzone

--- comandi/play.js
+++ comandi/play.js
@@ -13,6 +13,7 @@ const cliProgress = require('cli-progress');
 const querystring = require('node:querystring');
 const play = require('play-dl');
 const requisiti = require('../js/requisiti');
+const EventEmitter = require('node:events');
 
 const barStile = (nome)=>{
     if (nome)
@@ -159,14 +160,9 @@ const suona = async (server, channel, member) => {
 /*questi metodi devono ritornare una canzone o lista di oggetti Canzone*/
 /* canzone: {link, titolo, file} */
 
-const ricercaTitolo = async (song)=>{
+const ricercaTitolo = async (song, server,position,emitter, suonare=true)=>{
     try{
-        var pagina = await fetch(encodeURI(`https://www.youtube.com/results?${querystring.stringify({
-            search_query: `${song} lyrics`,
-            aqs: 'chrome.0.0i512j0i22i30l5.2443j0j15',
-            sourceid: 'chrome',
-            ie: 'UTF-8'
-        })}`));
+        var pagina = await fetch(encodeURI(`https://www.youtube.com/results?search_query=${song}`));
     }catch(error){
         console.error(error);
         throw error;
@@ -188,7 +184,9 @@ const ricercaTitolo = async (song)=>{
     }
     if (!token)
         throw errors.YouTubeTitleNotFound;
-    return trovaCanzoneYT(token);
+
+    const canzone = trovaCanzoneYT(token, server,position,emitter, suonare);
+    return canzone;
 }
 
 const parseSpotify = async(data)=>{
@@ -331,30 +329,41 @@ const spotifyPlaylist = async (id) =>{
     return titoli;
 }
 
-const updateDiscordBar = (interaction,bar)=>{
-    let ended = false;
-    if (interaction){
-        const updater = setInterval(()=>{
-            if (!ended)
-            interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                    .setTitle('Looking for songs')
-                    //.setDescription(`|${'\u2588'.repeat(Math.floor(bar.getProgress()*40))}${'\u2591'.repeat(Math.ceil((1-bar.getProgress())*40))}|`)
-                    .setDescription(`[${'='.repeat(Math.floor(bar.getProgress()*40))}${'-'.repeat(Math.ceil((1-bar.getProgress())*40))}]`)
-                    .setColor(Colori.default)
-                ]
-            });
-        },1750);
-        bar.on('stop', ()=>{
-            clearInterval(updater);
-            ended=true;
-            interaction=undefined;  
+const updateDiscordBar = (emitter,bar)=>{
+    const updater = setInterval(()=>{
+        emitter.emit('msg', {
+            embeds: [
+            new EmbedBuilder()
+                .setTitle('Looking for songs')
+                .setDescription(`|${'\u2588'.repeat(Math.floor(bar.getProgress()*40))}${'\u2591'.repeat(Math.ceil((1-bar.getProgress())*40))}|`)
+                //.setDescription(`[${'='.repeat(Math.floor(bar.getProgress()*40))}${'-'.repeat(Math.ceil((1-bar.getProgress())*40))}]`)
+                .setColor(Colori.default)
+            ]
         });
-    }
+    },1750);
+    bar.on('stop', ()=>{
+        clearInterval(updater);
+    });
 }
 
-const trovaLinkSpotify = async(id, resource, interaction)=>{
+cercaNomeCollection = async (id,emitter, resource)=>{
+    const res = await fetch(`https://api.spotify.com/v1/${{playlist:'playlists', album:'albums', artist:'artists'}[resource]}/${id}?market=IT`,{
+        headers: {
+            'Authorization': `Bearer ${spotifyToken}`
+        }
+    });
+    const data = await res.json();
+
+    if (res.status == 401 && data.error.message=='The access token expired'){
+        await getSpotifyToken();
+        return (cercaNomeCollection(id,emitter,resource));
+    }
+    await parseSpotify(data);
+
+    await emitter.emit('collectionTitle', data.name);
+}
+
+const trovaLinkSpotify = async(id, resource, server,position,emitter)=>{
     const titoli = [];
     if (!spotifyToken){
         await getSpotifyToken();
@@ -365,12 +374,15 @@ const trovaLinkSpotify = async(id, resource, interaction)=>{
             titoli.push(await spotifyTrack(id));
             break;
         case 'album':
+            await cercaNomeCollection(id,emitter,resource);
             titoli.push(... (await ( spotifyAlbum(id))));
             break;
         case 'artist':
+            await cercaNomeCollection(id,emitter,resource);
             titoli.push(... (await ( spotifyArtist(id))));
             break;
         case 'playlist':
+            await cercaNomeCollection(id,emitter,resource);
             titoli.push(... (await ( spotifyPlaylist(id))));
             break;
     }
@@ -384,60 +396,58 @@ const trovaLinkSpotify = async(id, resource, interaction)=>{
 
     const bar = new cliProgress.SingleBar(barStile('resolving titles'), cliProgress.Presets.shades_classic);
     bar.start(titoli.length, 0);
-    updateDiscordBar(interaction,bar);
+    updateDiscordBar(emitter,bar);
 
     for (let batch of titoliBatch){
-        canzoni.push(
-
-            ... (await Promise.all(
-                batch.map(titolo=>{
-                    return ( async () => {
-                        try{
-                            var canzone = await ricercaTitolo(titolo);
-                        }catch(e){
-                            console.error(e);
-                            if (e==errors.YouTubeKeyExpired)
-                                throw e
-                        }
-                        bar.increment();
-                        return canzone;
-                    } )()
-                })
-            )
-        ).filter(a=>a));
+        const trovate = (await Promise.all(
+            batch.map(titolo=>{
+                return ( async () => {
+                    try{
+                        var canzone = await ricercaTitolo(titolo, server,position,emitter, false);
+                    }catch(e){
+                        console.error(e);
+                        if (e==errors.YouTubeKeyExpired)
+                            throw e
+                    }
+                    bar.increment();
+                    return canzone;
+                } )()
+            }))
+        ).filter(a=>a);
+        canzoni.push(...trovate);
+        server.queue.splice(position+canzoni.length,0,...trovate);
+        if (titoliBatch.indexOf(batch)==0)
+            await emitter.emit('firstFound');
     }
     bar.stop();
-
-    // fare tutte le chiamate simultaneamente fa generare errori eccessivi
-    /*const bar = new cliProgress.SingleBar(barStile('Ricerca canzoni'), cliProgress.Presets.shades_classic);
-    bar.start(titoli.length,0);
-
-    const canzoni = (await Promise.all(
-                titoli.map(titolo=>{
-                    return ( async () => {
-                        try{
-                            var canzone = await ricercaTitolo(titolo);
-                        }catch(e){console.error(e);}
-                        bar.increment();
-                        return canzone;
-                    } )();
-                })
-            )).filter(a=>a)
-    bar.stop();*/
 
     return canzoni;
 }
 
-const trovaListaYT = async (videoId, listId, interaction)=>{
+const trovaListaYT = async (videoId, listId, server,position,emitter)=>{
     const ret = [];
     let pageToken;
     const bar = new cliProgress.SingleBar(barStile('ricerca canzoni'), cliProgress.Presets.shades_classic);
-    updateDiscordBar(interaction,bar);
+    updateDiscordBar(emitter,bar);
+
+    fetch('https://www.googleapis.com/youtube/v3/playlists?'+querystring.stringify({
+        part: 'snippet',
+        id: listId
+    }))
+    .then(res=>res.json())
+    .then(data=>{
+        emitter.emit('collectionTitle', data.items[0].snippet.title);
+    })
+    .catch(error=>{
+        console.error(error);
+    })
+
 
     let primo = true;
+    let firstFound=false;
     do {
         var hasNextPage = false;
-        const res = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5${pageToken?`&pageToken=${pageToken}`:''}&playlistId=${listId}&key=${youtubeKey}`);
+        const res = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50${pageToken?`&pageToken=${pageToken}`:''}&playlistId=${listId}&key=${youtubeKey}`);
 
         if (res.ok){
             const snippet = await res.json();
@@ -455,14 +465,23 @@ const trovaListaYT = async (videoId, listId, interaction)=>{
             if (!items){
                 throw new Error(errors.YouTubePlaylistNotFound);
             }
-            ret.push(...items.map(item=>{
-                bar.increment();
-                return {
+
+            const canzoni = items.map((item)=>{
+                const canzone = {
                     link: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
                     titolo: item.snippet.title,
                     file: false
-                }
-            }));
+                };
+                bar.increment();
+                return canzone;
+            });
+
+            server.queue.splice(position+ret.length,0,...canzoni);
+            ret.push(...canzoni);
+            if (!firstFound){
+                firstFound=true;
+                await emitter.emit('firstFound');
+            }
         }else{
             throw errors.YouTubeKeyExpired;
         }
@@ -471,7 +490,7 @@ const trovaListaYT = async (videoId, listId, interaction)=>{
     return ret;
 }
 
-const trovaCanzoneYT = async (videoId)=>{
+const trovaCanzoneYT = async (videoId, server, position, emitter, suonare=true)=>{
     const res = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${youtubeKey}`);
 
     if (res.ok){
@@ -480,22 +499,27 @@ const trovaCanzoneYT = async (videoId)=>{
         if (!item){
             throw new Error(errors.YouTubeVideoNotFound);
         }
-        return {
+        const song = {
             link: `https://www.youtube.com/watch?v=${item.id}`,
             titolo: item.snippet.title,
             file: false
         };
+        if (suonare) {
+            server.queue.splice(position,0,song);
+            await emitter.emit('firstFound');
+        }
+        return song;
     }else{
         throw errors.YouTubeKeyExpired;
     }
 }
 
 //deve ritornare un lista di canzoni
-const trovaCanzoni = async (song, interaction)=>{
+const accodaCanzoni = async (song, server, position, emitter)=>{
     if (song.match(/^https:\/\/youtu\.be\/.{11}$|^https:\/\/(www\.)?youtube\.com\/watch\?v=.{11}$/)){
         const match = song.match(/^https:\/\/youtu\.be\/(?<videoId>.{11})$|^https:\/\/(www\.)?youtube\.com\/watch\?v=(?<videoId2>.{11})$/);
         const videoId = match.groups.videoId || match.groups.videoId2;
-        return [await trovaCanzoneYT(videoId)];
+        return [await trovaCanzoneYT(videoId, server,position,emitter)];
     }
     // https://www.youtube.com/watch?v=QN1odfjtMoo&list=PLG7bQTXLuEouQFSnPUY6mFuJRf7ULbZbo
     // https://youtube.com/playlist?list=PLvwkDL8hMpWr2hyMgQwj9wHQINqwpTqWc
@@ -503,15 +527,15 @@ const trovaCanzoni = async (song, interaction)=>{
         const match = song.match(/^https:\/\/(www\.)?youtube\.com\/watch\?v=(?<videoId>.{11})&list=(?<listId>.*)$|^https:\/\/(www\.)?youtube.com\/playlist\?list=(?<listId2>.{34})$/);
         const videoId = match.groups.videoId;
         const listId = match.groups.listId || match.groups.listId2;
-        return await trovaListaYT(videoId, listId, interaction);
+        return await trovaListaYT(videoId, listId, server,position,emitter);
     }
     if (song.match(/^https:\/\/open\.spotify\.com\/(track|album|artist|playlist)\/.{22}/)){
         const match = song.match(/https:\/\/open\.spotify\.com\/(?<resource>track|album|artist|playlist)\/(?<id>.{22})/);
         const id =  match.groups.id;
         const resource = match.groups.resource;
-        return await trovaLinkSpotify(id, resource, interaction);
+        return await trovaLinkSpotify(id, resource, server,position,emitter);
     }
-    return [await ricercaTitolo(song)];
+    return [await ricercaTitolo(song, server,position,emitter)];
 }
 
 const saluta = async (connection)=>{
@@ -534,22 +558,35 @@ const saluta = async (connection)=>{
     await finito;
 }
 
-const join = async () => {
-    
-}
-
-const comando = async (song,position, member,channel, interaction)=>{
+const comando = async (song,position, member,channel, emitter)=>{
     const sameVCError = requisiti.sameVoiceChannel(member);
     if (sameVCError)
         return sameVCError;
 
     const guild = member.guild;
 
+    if (!servers.has(guild.id))
+        servers.set(guild.id, new Server(guild));
+    const server = servers.get(guild.id);
+    if (server.timeout){
+        clearTimeout(server.timeout);
+        server.timeout=undefined;
+    }
+
+    emitter.on('firstFound',()=>{
+        if (!server.isPlaying)
+            suona(server, channel, member);
+    });
+
+    let titolo;
+    emitter.on('collectionTitle', (collectionTitle)=>{
+        titolo = collectionTitle;
+    })
+
+    const posizione = ((!position || position==-1) && position!=0)? server.queue.length : Math.min(Math.max(position,0), server.queue.length);
     // cerca la canzone (o le canzoni) e ritorna un messaggio d'errore se non si trova nulla
     try {
-        var canzoni = await trovaCanzoni(song, interaction);
-        for (let canzone of canzoni)
-            console.log(canzone.titolo);
+        var canzoni = await accodaCanzoni(song, server,posizione, emitter);
     }catch(error){
         let errorMsg;
         switch (error){
@@ -586,22 +623,6 @@ const comando = async (song,position, member,channel, interaction)=>{
         }
     }
 
-    if (!servers.has(guild.id))
-        servers.set(guild.id, new Server(guild));
-    const server = servers.get(guild.id);
-    if (server.timeout){
-        clearTimeout(server.timeout);
-        server.timeout=undefined;
-    }
-
-    const posizione = ((!position || position==-1) && position!=0)? server.queue.length : Math.min(Math.max(position,0), server.queue.length);
-    server.queue = [...server.queue.slice(0,posizione), ...canzoni, ...server.queue.slice(posizione)];
-
-    // se il server non sta suonando nulla, inizia a suonare, altrimenti accoda e basta
-    if (!server.isPlaying){
-        await suona(server, channel,member);
-    }
-
     if (canzoni.length == 1)
         return {
             embeds: [
@@ -616,6 +637,7 @@ const comando = async (song,position, member,channel, interaction)=>{
             embeds: [
                 new EmbedBuilder()
                 .setTitle(`Queued ${canzoni.length} songs from position ${posizione+1}`)
+                .setDescription(`__[${titolo||song}](${song})__`)
                 .setColor(Colori.default)
             ]
         };
@@ -660,8 +682,11 @@ module.exports = {
 
             await interaction.deferReply({ephemeral:false});
 
-            const response = await comando(song, position, interaction.member,interaction.channel, interaction);
-            response.ephemeral = true;
+            const emitter = new EventEmitter();
+            emitter.on('msg', (msg)=>{
+                interaction.editReply(msg);
+            })
+            const response = await comando(song, position, interaction.member,interaction.channel, emitter);
             return await interaction.editReply(response);
         },
 
@@ -675,8 +700,20 @@ module.exports = {
 
     //        return message.channel.send({embeds:[new EmbedBuilder().setTitle('Error!').setDescription("No song specified.\nUse the `help` command to know more").setColor(Colori.error)]});
 
-            const response = await comando(canzone, undefined, message.member,message.channel);
-            return await message.channel.send(response);
+            const messaggio = await message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                    .setTitle('Looking for songs')
+                    .setColor(Colori.default)
+                ]
+            });
+            const emitter = new EventEmitter();
+            emitter.on('msg',(msg)=>{
+                messaggio.edit(msg);
+            });
+            const response = await comando(canzone, undefined, message.member,message.channel, emitter);
+            
+            return await messaggio.edit(response);
         },
 
         example: '`-play` `song` `[postition]`',
